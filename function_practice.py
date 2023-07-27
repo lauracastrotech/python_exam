902e77e44afbc8f20eeb8a1bea67f5735c06cd82
Laura Castro <castrobits3@gmail.com>
2023-07-26 20:12:42 -0400

    added day_of_year function

--- function_practice.py
+++ function_practice.py
@@ -36,7 +36,23 @@ def get_days_in_month(year, month):
     else:
         return days_in_month[month - 1]
     
+def day_of_year(year, month, day):
+    # List of days in each month (0 index represents January)
+    days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    
+    # Change day value from 28 to 29 in the days in month list when the year is a leap year
+    if month == 2 and is_leap_year(year):
+        days_in_month[1] = 29
+        
+    # Write conditional statement that ensures the month and day values are valid
+    if (month < 1 or month > 12) or (day < 1 or day > 31):
+        return None
+    
+    # Calculate the day of the year based on the year, month, and day
+    day_of_year = sum(days_in_month[:month - 1]) + day
+    
+    return day_of_year
+
 #Test Case 1
-print(is_leap_year(1989))
-print(get_days_in_month(1989, 11))
-print("save changes")
+print(day_of_year(2000, 12, 31))
+print(day_of_year(1989, 1, 12))
