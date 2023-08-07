35fde739c34f4881d761afc65349f800269556a5
Laura Castro <castrobits3@gmail.com>
2023-08-07 19:46:00 -0400

    fibonacci

---
+++ fibonacci.py
@@ -0,0 +1,9 @@
+# This is a code sample from section 4.5.1.6 Creating functions: Fibonacci numbers
+# Programmer: L Castro
+# Date: 8/7/2023
+
+# Write a function that takes an integer as an argument
+def fib(n):
+    # if the integer is less than 1, than it is invalid
+    if n < 1:
+        return None
